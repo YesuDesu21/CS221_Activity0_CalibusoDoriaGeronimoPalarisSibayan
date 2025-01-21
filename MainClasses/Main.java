d65f3f205b6c561ec8bcfd375d3a20456e7e8f6f
Cy Kbz <144660285+Cy-Kbz1@users.noreply.github.com>
2025-01-21 16:56:16 +0800

    Update Main.java

--- MainClasses/Main.java
+++ MainClasses/Main.java
@@ -1,4 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         DataManager swimmingDataManager = new DataManager();
