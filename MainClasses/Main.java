b8db32b52b8f5d970fd6a69c97f14b499feae96f
Yesu_Desu <144157115+YesuDesu21@users.noreply.github.com>
2025-01-21 11:59:57 +0800

    Update Main.java

--- MainClasses/Main.java
+++ MainClasses/Main.java
@@ -1,11 +1,62 @@
-import java.util.List;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         DataManager swimmingDataManager = new DataManager();
         swimmingDataManager.setReferenceListFromCSV("data.csv");
-        swimmingDataManager.filterByRank();
-        swimmingDataManager.searchByName();
-        swimmingDataManager.filterByTop5Events();
+
+
+
+
+        int choice;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome User to our Data Processing Program!");
+
+        System.out.println("Choose your option.");
+        while(true){
+            try {
+
+                System.out.println("""
+                        Options:
+                        1. Search by Athlete Name
+                        2. Display list of swimmers
+                        3. Filter by rank
+                        4. Sort by Year
+                        5. Top 5 fastest Results per Event
+                        """);
+                choice = sc.nextInt();
+                if(choice < 0){
+                    System.out.println("Input a positive number...");
+                }
+                switch(choice){
+                    case 1:
+                        swimmingDataManager.searchByName();
+                        break;
+                    case 2:
+                        swimmingDataManager.displayListOfSwimmers();
+                        break;
+                    case 3:
+                        swimmingDataManager.filterByRank();
+                        break;
+                    case 4:
+                        //method 4
+                        System.out.println("4");
+                        break;
+                    case 5:
+                        swimmingDataManager.filterByTop5Events();
+                        break;
+                }
+                System.out.println("Would you like to choose another option?[y/n]:");
+                String res = sc.next();
+                if(res.equalsIgnoreCase("n")){
+                    System.out.println("Thank you for using the program. Goodbye!");
+                    break;
+                } else if (res.equalsIgnoreCase("y")) {
+                    System.out.println("You can choose another option...");
+                }
+            }catch(NumberFormatException e){
+                System.out.println("Your choice is invalid, please input a number...");
+            }
+        }//End of while loop
     }
 }
